c684776cbba8681757905a775de82b1e1429699c
Lalu423 <>
2023-04-25 21:05:02 -0400

    added bootstrap and jquery script in HTML

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,21 @@
-var $SubmitBtn = $('#submitbtn');
-var Input = document.querySelector('#input')
-var cityName = document.querySelector('#city-name');
-var tempEl = document.querySelector('#temp');
-var winEl = document.querySelector('#wind');
-var humidEl = document.querySelector('#humid');
-var indexEl = document.querySelector('#index');
-
-var historyCities = $(JSON.parse(localStorage.getItem('saved')));
-
-saveCity();
-function saveCity() {
-    historyCities.sort();
-    for (var i = 0; i < historyCities.length; i++){
-        if(historyCities[i] ===  historyCities[i+1]){
-            historyCities.spice(i, 1);
-            i--;
-        }
-    }
-    btnCreator();
-}
-
-function btnCreator() {
-    btnPlace.children().remove();
-    
-}
-
 
 //gecoding API
-function getCity(inputFromField){
-    var url="http://api.openweathermap.org/geo/1.0/direct?" + inputFromField + "q={city name},{state code},{country code}&limit={limit}&appid={API key"
-        //fetch(url){
-        // ...
-        //}
-        //.then(data){
-        //data=[]
-            var lat = data[0].lat;
-            var lon = data[0].lon
-            getCurrentWeather(lat,lon);
-        //}
-    }
+//function getCity(inputFromField) {
+  //  var url = "http://api.openweathermap.org/geo/1.0/direct?" + inputFromField + "q={city name},{state code},{country code}&limit={limit}&appid={API key"
+    //fetch(url){
+    // ...
+    //}
+    //.then(data){
+    //data=[]
+  //  var lat = data[0].lat;
+   // var lon = data[0].lon
+    //getCurrentWeather(lat, lon);
+    //}
+//}
 
 //current weather
-function getCurrentWeather(lat, lon){
-    https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-}
+//function getCurrentWeather(lat, lon) {
+    //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+//}
 
-getCity
+//getCity
